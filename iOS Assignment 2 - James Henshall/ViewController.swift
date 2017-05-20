42493105f879f56a31810dbe39117c5a17584d01
Fiona <Fiona@fionas-air.lan>
2017-05-20 14:26:00 +1000

    Initial Commit

---
+++ iOS Assignment 2 - James Henshall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Assignment 2 - James Henshall
+//
+//  Created by Fiona on 20/5/17.
+//  Copyright © 2017 Fiona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
