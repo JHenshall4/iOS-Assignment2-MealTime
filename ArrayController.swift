f904873fc5c4be01a49a37b436e7a01b3d4f10a8
Fiona <Fiona@fionas-air.lan>
2017-05-23 00:42:17 +1000

    ....

---
+++ ArrayController.swift
@@ -0,0 +1,22 @@
+//
+//  ArrayController.swift
+//  iOS Assignment 2 - James Henshall
+//
+//  Created by Fiona on 23/5/17.
+//  Copyright © 2017 Fiona. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArrayController {
+    //Image array (will store image data, then convert back to image)
+    var Array0: [Data] = []
+    //Title array
+    var Array1: [String] = []
+    //Description array
+    var Array2: [String] = []
+    
+    static let sharedInstance = ArrayController()
+    
+}
